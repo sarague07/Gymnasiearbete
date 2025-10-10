484d13390e348773fe5f74f4d3e011c40d3a1ee6
Sara <sara.guellil@elev.ga.lbs.se>
2025-10-10 09:55:51 +0200

    player controller

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -4,9 +4,19 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Refrences")]
     private CharacterController controller;
+    [SerializeField] private Transform camera;
+ 
 
     [Header("Movement Settings")]
     [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 10f;
+    [SerializeField] private float sprintTransit = 5f;
+    [SerializeField] private float turningSpeed = 2f;
+    [SerializeField] private float gravity = 9.01f;
+    [SerializeField] private float jumpHeight = 2f;
+
+    private float verticalVelocity;
+    private float speed;
 
     [Header("Input")]
     private float moveInput;
@@ -26,15 +36,60 @@ public class PlayerController : MonoBehaviour
     private void Movement()
     {
         GroundMovement();
+        Turn();
     }
 
     private void GroundMovement()
     {
         Vector3 move =  new Vector3 (turnInput, 0, moveInput);
-        move.y = 0;
-        move *= walkSpeed;
+        move = camera.transform.TransformDirection(move);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = Mathf.Lerp(speed, sprintSpeed, sprintTransit * Time.deltaTime);
+        }
+        else
+        {
+            speed = Mathf.Lerp(speed, walkSpeed, sprintTransit * Time.deltaTime);
+        }
+
+        move *= speed;
+
+        move.y = VerticalForceCalculation();
+
 
         controller.Move(move * Time.deltaTime);
+    }
+
+   private void Turn()
+    {
+    if (Mathf.Abs(turnInput) > 0 || Mathf.Abs(moveInput) > 0)
+        {
+            Vector3 currentLookDirection = camera.forward;
+            currentLookDirection.y = 0;
+
+            Quaternion targetRotation = Quaternion.LookRotation(currentLookDirection);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turningSpeed);
+        }
+    }
+
+    private float VerticalForceCalculation()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -1f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = Mathf.Sqrt(jumpHeight * gravity * 2);
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        return verticalVelocity;
+
     }
 
     private void InputManagement()
